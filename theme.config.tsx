ccdfbedb4b3983da52baff724e7ca510890dbc12
Nathan Ramsdell <ramsdell.28@gmail.com>
2023-01-31 00:27:26 -0500

    Updated some pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Nathan Ramsdell</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/nathanramsdell/nathanramsdell',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/nathanramsdell/nathanramsdell',
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nathan Ramsdell's Documentation",
   },
 }
 
